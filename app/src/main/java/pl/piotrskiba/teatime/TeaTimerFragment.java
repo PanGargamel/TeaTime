c8c432679b541ef87d1efca0e1e14af640b4ab5a
PanGargamel <pskiba37@gmail.com>
2018-12-31 01:15:41 +0100

    Fixed timer text not being displayed

--- app/src/main/java/pl/piotrskiba/teatime/TeaTimerFragment.java
+++ app/src/main/java/pl/piotrskiba/teatime/TeaTimerFragment.java
@@ -101,6 +101,9 @@ public class TeaTimerFragment extends Fragment implements SeekBar.OnSeekBarChang
                 int progress = (int)((float)timeleft/mTotalBrewingTime*1000);
                 mTimerProgressBar.setProgress(progress);
             }
+            else{
+                setDefaultSeekBarProgress();
+            }
         }
         else {
             setDefaultSeekBarProgress();
